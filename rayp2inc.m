8fcf760757b31ca617e4ec72f4353d4856ee6ee2
Zach Eilon <zeilon@ldeo.columbia.edu>
2014-03-27 10:14:27 -0400

    finished rayp2inc

--- rayp2inc.m
+++ rayp2inc.m
@@ -1,7 +1,15 @@
-function [ output_args ] = rayp2inc( input_args )
-%RAYP2INC Summary of this function goes here
-%   Detailed explanation goes here
+function [ inc ] = rayp2inc( rayp,V,z )
+% [ inc ] = rayp2inc( rayp,v,z )
+% Convert a ray parameter in seconds per degre to an incidence angle, using
+% the velocity, V (km/s) and a depth, z (km)
+% IF no depth is given, assume this is a flat Earth and rayp is in s/km
 
+if nargin>2
+    inc = asind( (r2d(rayp) .* V)./(6371-z) );
+end
+if nargin < 3
+    inc = asind( rayp.*V );
+end
 
 end
 
