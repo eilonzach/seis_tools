0eb1098d5300d525336907ef38d4078f4ac2d3a1
Zach Eilon <zeilon@ldeo.columbia.edu>
2014-09-18 19:55:35 -0400

    edited rayp2inc to make consistent with the one in seizmo-master

--- rayp2inc.m
+++ rayp2inc.m
@@ -1,13 +1,17 @@
-function [ inc ] = rayp2inc( rayp,V,z )
-% [ inc ] = rayp2inc( rayp,v,z )
-% Convert a ray parameter in seconds per degre to an incidence angle, using
-% the velocity, V (km/s) and a depth, z (km)
+function [ inc ] = rayp2inc( rayp,V,r )
+% [ inc ] = rayp2inc( rayp,v,r )
+% Convert a ray parameter in seconds per degree to an incidence angle in 
+% degrees, using the velocity, V (km/s) and a radius, r (km)
 % IF no depth is given, assume this is a flat Earth and rayp is in s/km
 
-if nargin>2
-    inc = asind( (r2d(rayp) .* V)./(6371-z) );
-end
 if nargin < 3
+    r = 6371;
+end
+z = 6371-r;
+
+if nargin > 2
+    inc = asind( (r2d(rayp) .* V)./(6371-z) );
+else
     inc = asind( rayp.*V );
 end
 
